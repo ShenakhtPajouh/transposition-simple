e70411c16324c5450e4babdb7292e4a3f51f5c77
fhn98 <fahim.hosseini.77@gmail.com>
2019-01-10 12:05:48 +0330

    new changes in utils

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 import utils
 import main_model
 
 
 def train(embedding_table, batch_size, epochs, learning_rate, hidden_len, encoder_dropout, classifier_dropout, data,
-          save_path):
+           validation_split ,  save_path):
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     session = tf.Session(config=config)
@@ -23,23 +24,51 @@ def train(embedding_table, batch_size, epochs, learning_rate, hidden_len, encode
                                                      save_weights_only=True,
                                                      verbose=1)
 
-    dataset_12 = tf.data.Dataset.from_tensor_slices((data[0], data[1]))
-    dataset_label = tf.data.Dataset.from_tensor_slices(data[2])
+    #dataset_12 = tf.data.Dataset.from_tensor_slices((data[0], data[1]))
+    #dataset_label = tf.data.Dataset.from_tensor_slices(data[2])
+    #dataset = tf.data.Dataset.zip((dataset_12, dataset_label)).batch(batch_size, drop_remainder=True)
 
-    dataset = tf.data.Dataset.zip((dataset_12, dataset_label)).batch(batch_size, drop_remainder=True)
+    #val_dataset_12 = tf.data.Dataset.from_tensor_slices((val_data[0], val_data[1]))
+    #val_dataset_label = tf.data.Dataset.from_tensor_slices(val_data[2])
+    #val_dataset = tf.data.Dataset.zip((val_dataset_12, val_dataset_label)).batch(batch_size , drop_remainder=True)
 
-    model.fit(dataset, epochs=epochs, steps_per_epoch=5, callbacks=[cp_callback])
+    history = model.fit(x = [data[0],data[1]] , y = data[2] , batch_size = batch_size, validation_split = validation_split ,epochs=epochs, callbacks=[cp_callback])
+
+    return history
+
+
+ def plot (name , history):
+        plt.figure(figsize=(16, 10))
+        val = plt.plot(history.epoch, history.history['val_binary_crossentropy'],
+                       '--', label=name+' Val')
+        plt.plot(history.epoch, history.history['binary_crossentropy'], color=val[0].get_color(),
+                 label=name+' Train')
+        plt.xlabel('Epochs')
+        plt.ylabel('binary_crossentropy'.replace('_', ' ').title())
+        plt.legend()
+        plt.xlim([0, max(history.epoch)])
 
 
 if __name__ == '__main__':
     stoi = utils.get_from_file('stoi.pkl')
     vectors = utils.get_from_file('vectors.pkl')
 
-    data = utils.get_from_file('data.pkl')
+    first = utils.get_from_file('first.pkl')
+    second = utils.get_from_file('second.pkl')
+    labels = utils.get_from_file('labels.pkl')
 
-    train_data = (data[0][:(data[0].shape[0] // 10) * 7, :], data[1][:(data[1].shape[0] // 10) * 7, :],
-                  data[2][:(data[2].shape[0] // 10) * 7, :])
+    train_data = (first[:(first.shape[0] // 10) * 9, :], second[:(second.shape[0] // 10) * 9, :],
+                  labels[:(labels.shape[0] // 10) * 9, :])
+    test_data = (first[(first.shape[0] // 10) * 9:, :],
+                second[(second.shape[0] // 10) * 9:, :],
+                labels[(labels.shape[0] // 10) * 9:, :])
 
-    train(embedding_table=vectors, batch_size=64, epochs=10, learning_rate=0.001, hidden_len=200,
-          encoder_dropout=0.5, classifier_dropout=0.2, data=train_data, save_path='./model.ckpt')
+
+
+    history = train(embedding_table=vectors, batch_size=64, epochs=50, learning_rate=0.001, hidden_len=200,
+          encoder_dropout=0.5, classifier_dropout=0.2 , validation_split = 0.05, data=train_data, save_path='./model.ckpt')
+
+
+    plot ('LSTM + FC' , history)
+
 
--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from remote.API import get_paragraphs
 import numpy as np
 
 
-def extract_embedding_from_dict(dict_path , vocab_size , embedding_len , vectors_path , stoi_path , itos_path):
+def extract_embedding_from_dict(dict_path, vocab_size, embedding_len, vectors_path, stoi_path, itos_path):
     """
     :param dict_path: path of the dictionary of word to embedding
     :param vocab_size: size of embedding vocabulary
@@ -12,96 +12,119 @@ def extract_embedding_from_dict(dict_path , vocab_size , embedding_len , vectors
     """
 
     # an np array of shape [vocab_size , embedding_len] containing embedding vectors
-    vectors = np.zeros ((vocab_size, embedding_len) , dtype=np.float32)
-    #a dictionary that maps words to their corresponding index in vectors
+    vectors = np.zeros((vocab_size, embedding_len), dtype=np.float64)
+    # a dictionary that maps words to their corresponding index in vectors
     stoi = dict()
-    #a list in which index of each word is its index in vectors
+    # a list in which index of each word is its index in vectors
     itos = []
 
-    i=0
-    with open (dict_path , 'r' , encoding= 'UTF-8') as infile:
+    i = 0
+    with open(dict_path, 'r', encoding='UTF-8') as infile:
         for line in infile.readlines():
             row = line.strip().split(' ')
             itos.append(row[0])
             listt = [float(j) for j in row[1:]]
-            stoi[row[0]]=i
-            vectors[i]=np.array(listt)
-            i+=1
+            stoi[row[0]] = i
+            vectors[i] = np.array(listt)
+            i += 1
 
-    with open (vectors_path, 'wb') as pkl:
-        pickle.dump(vectors , pkl)
-    with open (stoi_path , 'wb') as pkl:
-        pickle.dump(stoi , pkl)
-    with open (itos_path , 'wb') as pkl:
-        pickle.dump(itos , pkl)
+    with open(vectors_path, 'wb') as pkl:
+        pickle.dump(vectors, pkl)
+    with open(stoi_path, 'wb') as pkl:
+        pickle.dump(stoi, pkl)
+    with open(itos_path, 'wb') as pkl:
+        pickle.dump(itos, pkl)
 
 
-def get_from_file (path):
+def get_from_file(path):
     """
     :param path: path of the file
     :return: object read from file
     """
     with open(path, 'rb') as pkl:
-        ret = pickle.load(pkl)
+        ret = pickle.load(pkl, encoding='iso-8859-1')
 
     return ret
 
-def get_paragraphs_as_words(stoi , paragraph_id=None, books=None, tags=None,
-                 num_sequential=2, Paragraph_Object=False):
+
+def get_paragraphs_as_words(stoi, paragraph_id=None, books=None, tags=None,
+                            num_sequential=2, Paragraph_Object=True):
     """
     :return: a triple of np arrays: first: embedding of words of first paragraphs per row
                                     second: embedding of words of second paragraphs per row
                                     labels: whether the first paragraph has happend before the second paragraph
     """
 
-    paragraphs = get_paragraphs(paragraph_id = paragraph_id, books=books,
-                                tags=tags, num_sequential=num_sequential, Paragraph_Object=Paragraph_Object)
-
-    max_len=-1
+    paragraphs = get_paragraphs(paragraph_id=paragraph_id, books=books,
+                                tags=tags, num_sequential=num_sequential, paragraph_object=Paragraph_Object)
 
     first_seq = []
     second_seq = []
 
-    for x,y in paragraphs:
-        for sent in x:
-            for word in sent:
-                first_seq.append(word)
-        for sent in y:
-            for word in sent:
-                second_seq.append(word)
+    max_len = -1
+    max_ind = 0
 
+    for x, y in paragraphs:
+        first_seq.append(x.text(format="words", lowercase=True))
+        second_seq.append(y.text(format="words", lowercase=True))
 
-    for seq in first_seq:
-        max_len = max(max_len , len(seq))
+    for i, seq in enumerate(first_seq):
+        if max_len < len(seq):
+            max_len = len(seq)
+            max_ind = i
 
-    for seq in second_seq:
-        max_len = max (max_len , len(seq))
+    for i, seq in enumerate(second_seq):
+        if max_len < len(seq):
+            max_len = len(seq)
+            max_ind = i
 
-    first = np.zeros((2*len(paragraphs),max_len),dtype=np.int32)
-    second = np.zeros((2*len(paragraphs), max_len), dtype=np.int32)
+    print(" ".join(first_seq[max_ind]))
+    print(" ")
+    print(" ".join(second_seq[max_ind]))
 
-    labels = np.zeros((2*len(paragraphs),2) , dtype=np.float64)
-    labels[:len(paragraphs),0]=1
-    labels[len(paragraphs):,1] = 1
+    first = np.zeros((2 * len(paragraphs), max_len), dtype=np.int32)
+    second = np.zeros((2 * len(paragraphs), max_len), dtype=np.int32)
 
-    for i ,seq in enumerate(first_seq):
-        for j , word in enumerate(seq):
-            first[i,j]=stoi[word]
+    labels = np.zeros((2 * len(paragraphs), 2), dtype=np.float32)
+    labels[:len(paragraphs), 0] = 1
+    labels[len(paragraphs):, 1] = 1
+    count = 0
 
-    for i ,seq in enumerate(second_seq):
-        for j , word in enumerate(seq):
-            second[i,j]=stoi[word]
+    for i, seq in enumerate(first_seq):
+        for j, word in enumerate(seq):
+            if word in stoi:
+                first[i, j] = stoi[word]
+            else:
+                count += 1
+                first[i, j] = stoi['**BLANK**']
 
-    first[len(paragraphs):,:]=second[:len(paragraphs),:]
+    for i, seq in enumerate(second_seq):
+        for j, word in enumerate(seq):
+            if word in stoi:
+                second[i, j] = stoi[word]
+            else:
+                count += 1
+                second[i, j] = stoi['**BLANK**']
+
+    print(max_len)
+
+    first[len(paragraphs):, :] = second[:len(paragraphs), :]
     second[len(paragraphs):, :] = first[:len(paragraphs), :]
 
-    permutation = np.random.permutation(2*len(paragraphs))
+    permutation = np.random.permutation(2 * len(paragraphs))
 
     first = first[permutation]
     second = second[permutation]
     labels = labels[permutation]
 
-    return first , second , labels
+    with open('first.pkl', 'wb') as pkl:
+        pickle.dump(first, pkl)
+    with open('second.pkl', 'wb') as pkl:
+        pickle.dump(second, pkl)
+    with open('labels.pkl', 'wb') as pkl:
+        pickle.dump(labels, pkl)
+
+    return first, second, labels
 
 
 

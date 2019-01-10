9ec6a0d95d88407e98346b07dac593b111357ccc
fhn98 <fahim.hosseini.77@gmail.com>
2019-01-10 15:24:53 +0330

    adding make_dataset_with_indices

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ import utils
 import main_model
 
 
-def train(embedding_table, batch_size, epochs, learning_rate, hidden_len, encoder_dropout, classifier_dropout, data,
-           validation_split ,  save_path):
+def train(embedding_table, batch_size, epochs, learning_rate, hidden_len, encoder_dropout, classifier_dropout, data, val_data ,
+           validation_split , save_path):
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     session = tf.Session(config=config)
@@ -24,20 +24,21 @@ def train(embedding_table, batch_size, epochs, learning_rate, hidden_len, encode
                                                      save_weights_only=True,
                                                      verbose=1)
 
-    #dataset_12 = tf.data.Dataset.from_tensor_slices((data[0], data[1]))
-    #dataset_label = tf.data.Dataset.from_tensor_slices(data[2])
-    #dataset = tf.data.Dataset.zip((dataset_12, dataset_label)).batch(batch_size, drop_remainder=True)
+    dataset_12 = tf.data.Dataset.from_tensor_slices((data[0], data[1]))
+    dataset_label = tf.data.Dataset.from_tensor_slices(data[2])
+    dataset = tf.data.Dataset.zip((dataset_12, dataset_label)).batch(batch_size, drop_remainder=True)
 
-    #val_dataset_12 = tf.data.Dataset.from_tensor_slices((val_data[0], val_data[1]))
-    #val_dataset_label = tf.data.Dataset.from_tensor_slices(val_data[2])
-    #val_dataset = tf.data.Dataset.zip((val_dataset_12, val_dataset_label)).batch(batch_size , drop_remainder=True)
+    val_dataset_12 = tf.data.Dataset.from_tensor_slices((val_data[0], val_data[1]))
+    val_dataset_label = tf.data.Dataset.from_tensor_slices(val_data[2])
+    val_dataset = tf.data.Dataset.zip((val_dataset_12, val_dataset_label)).batch(batch_size , drop_remainder=True)
 
-    history = model.fit(x = [data[0],data[1]] , y = data[2] , batch_size = batch_size, validation_split = validation_split ,epochs=epochs, callbacks=[cp_callback])
+    #history = model.fit(x = [data[0],data[1]] , y = data[2] , batch_size = batch_size, validation_split = validation_split ,epochs=epochs, callbacks=[cp_callback])
 
+    history = model.fit(dataset, epochs=10, steps_per_epoch=30,validation_data=val_dataset,validation_steps=3,callbacks=[cp_callback])
     return history
 
 
- def plot (name , history):
+def plot (name , history):
         plt.figure(figsize=(16, 10))
         val = plt.plot(history.epoch, history.history['val_binary_crossentropy'],
                        '--', label=name+' Val')
@@ -57,11 +58,14 @@ if __name__ == '__main__':
     second = utils.get_from_file('second.pkl')
     labels = utils.get_from_file('labels.pkl')
 
-    train_data = (first[:(first.shape[0] // 10) * 9, :], second[:(second.shape[0] // 10) * 9, :],
-                  labels[:(labels.shape[0] // 10) * 9, :])
-    test_data = (first[(first.shape[0] // 10) * 9:, :],
-                second[(second.shape[0] // 10) * 9:, :],
-                labels[(labels.shape[0] // 10) * 9:, :])
+    data = utils.make_dataset_with_indices(stoi , tags=[1,2])
+
+    print(data[3].shape)
+    #train_data = (first[:(first.shape[0] // 10) * 9, :], second[:(second.shape[0] // 10) * 9, :],
+    #             labels[:(labels.shape[0] // 10) * 9, :])
+    #test_data = (first[(first.shape[0] // 10) * 9:, :],
+    #           second[(second.shape[0] // 10) * 9:, :],
+    #           labels[(labels.shape[0] // 10) * 9:, :])
 
 
 
@@ -69,6 +73,6 @@ if __name__ == '__main__':
           encoder_dropout=0.5, classifier_dropout=0.2 , validation_split = 0.05, data=train_data, save_path='./model.ckpt')
 
 
-    plot ('LSTM + FC' , history)
+    #plot ('LSTM + FC' , history)
 
 
--- utils.py
+++ utils.py
@@ -127,6 +127,110 @@ def get_paragraphs_as_words(stoi, paragraph_id=None, books=None, tags=None,
     return first, second, labels
 
 
+def make_dataset_with_indices(stoi, paragraph_id=None, books=None, tags=None,
+                              num_sequential=2, Paragraph_Object=True):
+    """
+        :return: a tuple of four np arrays: first: embedding of words of first paragraphs per row
+                                        second: embedding of words of second paragraphs per row
+                                        labels: whether the first paragraph has happend before the second paragraph
+                                        indices: indices in which each sentence of paragraph ends
+        """
+
+    paragraphs = get_paragraphs(paragraph_id=paragraph_id, books=books,
+                                tags=tags, num_sequential=num_sequential, paragraph_object=Paragraph_Object)
+
+    first_sents = []
+    second_sents = []
+
+    max_len = -1
+    max_sent = -1
+
+    for x, y in paragraphs:
+        first_sents.append(x.text(format="sentences", lowercase=True))
+        second_sents.append(y.text(format="sentences", lowercase=True))
+
+    first_seq = []
+    second_seq = []
+
+    for i, seq in enumerate(first_sents):
+        max_sent = max(max_sent, len(seq))
+        l = []
+        for sent in seq:
+            l += sent
+        max_len = max(max_len, len(l))
+        first_seq.append(l)
+
+    for i, seq in enumerate(second_sents):
+        max_sent = max(max_sent, len(seq))
+        l = []
+        for sent in seq:
+            l += sent
+        max_len = max(max_len, len(l))
+        second_seq.append(l)
+
+    first = np.zeros((2 * len(paragraphs), max_len), dtype=np.int64)
+    second = np.zeros((2 * len(paragraphs), max_len), dtype=np.int64)
+
+    labels = np.zeros((2 * len(paragraphs), 2), dtype=np.float64)
+    labels[:len(paragraphs), 0] = 1
+    labels[len(paragraphs):, 1] = 1
+
+    indices1 = np.ones((2 * len(paragraphs), max_sent), dtype=np.int64) * (max_len + 1)
+    indices2 = np.ones((2 * len(paragraphs), max_sent), dtype=np.int64) * (max_len + 1)
+
+
+    for i, p in enumerate(first_sents):
+        l = -1
+        for j, sent in enumerate(p):
+            l += len(sent)
+            indices1[i][j] = l
+            indices2[len(paragraphs) + i, j] = l
+
+    for i, p in enumerate(second_sents):
+        l = -1
+        for j, sent in enumerate(p):
+            l += len(sent)
+            indices2[i][j] = l
+            indices1[len(paragraphs) + i, j] = l
+
+    for i, seq in enumerate(first_seq):
+        for j, word in enumerate(seq):
+            if word in stoi:
+                first[i, j] = stoi[word]
+            else:
+                first[i, j] = stoi['**BLANK**']
+
+    for i, seq in enumerate(second_seq):
+        for j, word in enumerate(seq):
+            if word in stoi:
+                second[i, j] = stoi[word]
+            else:
+                second[i, j] = stoi['**BLANK**']
+
+    first[len(paragraphs):, :] = second[:len(paragraphs), :]
+    second[len(paragraphs):, :] = first[:len(paragraphs), :]
+
+    permutation = np.random.permutation(2 * len(paragraphs))
+
+    first = first[permutation]
+    second = second[permutation]
+    labels = labels[permutation]
+    indices1 = indices1[permutation]
+    indices2 = indices2 [permutation]
+
+    with open('first.pkl', 'wb') as pkl:
+        pickle.dump(first, pkl)
+    with open('second.pkl', 'wb') as pkl:
+        pickle.dump(second, pkl)
+    with open('labels.pkl', 'wb') as pkl:
+        pickle.dump(labels, pkl)
+    with open('indices.pkl', 'wb') as pkl:
+        pickle.dump((indices1, indices2), pkl)
+
+    return first, second, labels, indices1, indices2
+
+
+
 
 
 

032d793418d08d5575363f1b2dc1fcc698ae2b43
Mohammad Reza Samsami <39796257+mrsamsami@users.noreply.github.com>
2019-02-04 15:29:01 +0330

    Update entity_main_model.py

--- entity_network/entity_main_model.py
+++ entity_network/entity_main_model.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 from tensorflow import keras
-from entity_network.staticRecurrentEntNet import StaticRecurrentEntNet
-from Simpler_Models.determiner import Determiner
-
+from staticRecurrentEntNet import simple_entity_network as entnet, EntityCell
+import sys
+sys.path.insert(0, '../Simpler_Models/')
+from determiner import Determiner
 
 class entity_main_model (keras.Model):
     def __init__(self, name, entity_num, entity_embedding_dim, rnn_hidden_size,max_sent_num , determiner_dropout_rate):
@@ -12,38 +13,26 @@ class entity_main_model (keras.Model):
         self._entity_embedding_dim = entity_embedding_dim
         self._rnn_hidden_size = rnn_hidden_size
         self._max_sent_num = max_sent_num
-
-        self._entnet = StaticRecurrentEntNet(self._entity_num, self._entity_embedding_dim,
-                                             self._rnn_hidden_size,self._max_sent_num , name=self._name+"/entnet")
-        self.determiner = Determiner(self._name+"/determiner", output_size=2, hidden_dim=64,
+        self._entity_cell = EntityCell(max_entity_num=entity_num, entity_embedding_dim=entity_embedding_dim, name='entity_cell')
+        self._determiner = Determiner(self._name+"/determiner", output_size=2, hidden_dim=64,
                                      dropout=determiner_dropout_rate)
 
     @property
     def variables(self):
-        return self._determiner.variables+self._entnet.variables
+        return self._determiner.variables + self._entity_cell.variables
 
     def call(self , inputs):
         """
         !!! here we concider the first sequence as concatenation of the first paragraph and the second paragraph
             and the second sequence as concatenation of the second paragraph and the first paragraph
 
-        :param inputs: inputs[0] = a tensor of shape [batch_size,max_sent_num,max_token_num,embedding_len] containing the
-                                   first sequence
-                       inputs[1] = a tensor of shape [batch_size,max_sent_num,max_token_num,embedding_len] containing the
-                                   second sequence
-                       inputs[2] = a tensor of shape [batch_size , max_sent_num , max token_num] containing the mask of
-                                   first sequence
-                       inputs[3] = a tensor of shape [batch_size , max_sent_num , max token_num] containing the mask of
-                                   first sequence
-                       inputs[4] = a tensor of shape [batch_size ,entity_num , entitiy_embedding_dim] containing embedding keys
+        :param inputs: inputs[0] = a tensor of shape [batch_size, max_sent_num, max_token_num, embedding_len] containing the sequence
+                       inputs[1] = a tensor of shape [batch_size , max_sent_num , max token_num] containing the mask of the sequence
+                       inputs[2] = a tensor of shape [entity_num , entitiy_embedding_dim] containing embedding keys
 
-        :return: a tensor of shape [batch_size , 2] containing result of classification
+        :return: a tensor of shape [batch_size , entity_num , 2] containing result of classification
         """
 
-        x = self._entnet([inputs[4], inputs[0], inputs[2]])
-        y = self._entnet ([inputs[4],inputs[1],inputs[3]])
-
-        ret = self._determiner(tf.concat((x,y),axis=-1))
-
+        x = entnet([inputs[0], inputs[1]], inputs[2], self._entity_cell)
+        ret = 1 - self._determiner(x)
         return ret
-

97827ab45516b0c5b1e658223abe086bb6fada1b
Mohammad Reza Samsami <39796257+mrsamsami@users.noreply.github.com>
2019-02-04 15:26:46 +0330

    Update batch_generator.py

--- entity_network/batch_generator.py
+++ entity_network/batch_generator.py
@@ -1,94 +1,119 @@
 import numpy as np
 import tensorflow as tf
-import json
-from BERT import tokenization
-from BERT import modeling
-import utils
+import pickle
+import time
+
+def f(a, b, a_len, b_len, batch_size, max_len, dim):
+    c = np.zeros((batch_size, 2*max_len, dim), dtype = np.float32)
+    mask = np.zeros((batch_size, 2*max_len), dtype = np.float32)
+    
+    for i in range(batch_size):
+        c[i][ : a_len[i]] = a[i][ : a_len[i]]
+        c[i][a_len[i] : a_len[i]+b_len[i]] = b[i][ : b_len[i]]
+        mask[i][ : a_len[i]+b_len[i]] = np.tile([1], a_len[i]+b_len[i])
+        
+    return c, mask
 
 
-class BERT_encoder:
-    """
-    a class for encoding paragraphs
-    """
-    def __init__(self , config_path , vocab_path , base_path):
-        """
-        making instance of encoder and bert model
-        """
+def validation_generator(batch_size):
+    pairs_encoded = []
+    for i in range(3):
+        path = '../paragraph_pairs_encoded' + str(19 + i) + '.pkl'
+        with open(path, 'rb') as pkl:
+            pairs_encoded.append(pickle.load(pkl))
+    
+    with open('../keys_encoded9.pkl', 'rb') as pkl:
+        keys = pickle.load(pkl)
+    keys = np.float32(np.append(keys, keys, axis = 0))
+    
+    with open('../mask_encoded9.pkl', 'rb') as pkl:
+        keys_mask = pickle.load(pkl)
+    keys_mask = np.float32(np.append(keys_mask, keys_mask, axis = 0))
+    
+    max_len = 37
+    dim = 768
+    encoded = np.empty(shape = [0, 2*max_len, dim], dtype = np.float32)
+    masks = np.empty(shape = [0, 2*max_len], dtype = np.float32)
+    labels = np.empty(shape = [0, ], dtype = np.int32)
 
-        self._base_path = base_path
+    for i in range(2):
+        for j in range(3):
+            e, mask = f(pairs_encoded[j][i], pairs_encoded[j][i^1], pairs_encoded[j][2+i], pairs_encoded[j][2+(i^1)], len(pairs_encoded[j][3]), max_len, dim)
+            encoded = np.append(encoded, e, axis = 0)
+            masks = np.append(masks, mask, axis = 0)
+            labels = np.append(labels, np.tile([i^1], len(pairs_encoded[j][3])))
+            
+    del pairs_encoded
+    
+    n = len(labels)
+    pi = np.random.permutation(n)
+    encoded = encoded[pi]
+    masks = masks[pi]
+    keys = keys[pi]
+    keys_mask = keys_mask[pi]
+    labels = labels[pi]
+    
+    for i in range(0, n, batch_size):
+        if i + batch_size > n:
+            break
+        yield (encoded[i:i + batch_size], masks[i:i + batch_size], keys[i:i + batch_size], keys_mask[i:i + batch_size], labels[i:i + batch_size])
+    
+    del labels
+    del keys_mask
+    del keys
+    del masks
+    del encoded
+    
 
-        self._tokenizer = tokenization.FullTokenizer(vocab_path)
+def batch_generator(batch_size):
+    for epoch in range(10):
+        for b in range(9):
+            pairs_encoded = []
+            for i in range(2):
+                path = '../paragraph_pairs_encoded' + str(2*b + 1 + i) + '.pkl'
+                with open(path, 'rb') as pkl:
+                    pairs_encoded.append(pickle.load(pkl))
 
-        with open(config_path) as f:
-            self._conf = json.loads(f.read())
+            path = '../keys_encoded' + str(b) + '.pkl'
+            with open(path, 'rb') as pkl:
+                keys = pickle.load(pkl)
+            keys = np.float32(np.append(keys, keys, axis = 0))
 
-        config = modeling.BertConfig(**self._conf)
+            path = '../mask_encoded' + str(b) + '.pkl'
+            with open(path, 'rb') as pkl:
+                keys_mask = pickle.load(pkl)
+            keys_mask = np.float32(np.append(keys_mask, keys_mask, axis = 0))
 
-        self._inputs = tf.placeholder(shape=[None, None], dtype=tf.int32)
-        self._mask = tf.placeholder(shape=[None, None], dtype=tf.int32)
-        self._model = modeling.BertModel(config=config, is_training=False, input_ids=self._inputs, input_mask=self._mask,
-                                   use_one_hot_embeddings=False)
+            max_len = 37
+            dim = 768
+            encoded = np.empty(shape = [0, 2*max_len, dim], dtype = np.float32)
+            masks = np.empty(shape = [0, 2*max_len], dtype = np.float32)
+            labels = np.empty(shape = [0, ], dtype = np.int32)
 
-        self._embeddings = self._model.get_all_encoder_layers()
+            for i in range(2):
+                for j in range(2):
+                    e, mask = f(pairs_encoded[j][i], pairs_encoded[j][i^1], pairs_encoded[j][2+i], pairs_encoded[j][2+(i^1)], len(pairs_encoded[j][3]), max_len, dim)
+                    encoded = np.append(encoded, e, axis = 0)
+                    masks = np.append(masks, mask, axis = 0)
+                    labels = np.append(labels, np.tile([i^1], len(pairs_encoded[j][3])))
 
-        config = tf.ConfigProto()
-        config.gpu_options.allow_growth = True
-        self._sess = tf.Session(config=config)
+            del pairs_encoded
 
-        saver = tf.train.Saver()
-        saver.restore(self._sess, self._base_path)
+            n = len(labels)
+            pi = np.random.permutation(n)
+            encoded = encoded[pi]
+            masks = masks[pi]
+            keys = keys[pi]
+            keys_mask = keys_mask[pi]
+            labels = labels[pi]
 
-    def encode(self ,batch):
-        """
-        :param batch: a list of paragraphs to be encoded
-        :return: - a numpy array of shape [batch_size*maximum_num_tokens]
-                 - a list of lists of tokens for each paragraph
-        """
+            for i in range(0, n, batch_size):
+                if i + batch_size > n:
+                    break
+                yield (encoded[i:i + batch_size], masks[i:i + batch_size], keys[i:i + batch_size], keys_mask[i:i + batch_size], labels[i:i + batch_size])
 
-        tokenized = []
-        tokenized_ids = []
-        for seq in batch:
-            tokens = self._tokenizer.tokenize(seq)
-            ids = self._tokenizer.convert_tokens_to_ids(tokens)
-
-            tokenized.append(tokens)
-            tokenized_ids.append(ids)
-
-        max_len = -1
-        for ls in tokenized_ids:
-            max_len = max (max_len , len(ls))
-
-        ins = np.zeros((len(batch),max_len) , dtype=np.int32)
-        mask = np.zeros((len(batch),max_len) , dtype=np.int32)
-
-        for i,ids in enumerate(tokenized_ids):
-            for j, id in enumerate(ids):
-                ins[i][j]=ids[j]
-                mask[i][j]=1
-
-        embds = self._sess.run(self._embeddings, {self._inputs: ins, self._mask: mask })
-
-        final_embd = embds[-1]+embds[-2]+embds[-3]+embds[-4]
-
-        return tokenized , final_embd
-
-
-
-def get_normal_batch(path , bert_config_path , bert_vocab_path , bert_base_path , batch_size):
-    paragraphs = utils.get_from_file(path)
-    encoder = BERT_encoder(bert_config_path , bert_vocab_path , bert_base_path)
-
-
-
-
-
-
-
-
-
-
-
-
-def get_batch(path , batch_size , mode):
-    if mode=='normal':
-        get_normal_batch(path , batch_size)
+            del labels
+            del keys_mask
+            del keys
+            del masks
+            del encoded
